000b4cb17a9fb3bd9cd7964115ea6bf920ba59e6
Francis <hisconer@gmail.com>
2017-04-21 10:13:58 +0200

    Remove icons sketch file

--- query-selection.sketchplugin/Contents/Sketch/query-selection.js
+++ query-selection.sketchplugin/Contents/Sketch/query-selection.js
@@ -1,7 +1,7 @@
 // Functions
 const selectLayers = layers => layers.slice().map(layer => layer.select_byExpandingSelection(true, true));
 const deselectLayers = context => context.document.currentPage().deselectAllLayers();
-
+const selectedLayers = context => context.selection.slice();
 const isShape = layer => layer.class() == "MSShapeGroup";
 const isText = layer => layer.class() == "MSTextLayer";
 const isImage = layer => layer.class() == "MSBitmapLayer";
@@ -10,27 +10,23 @@ const isGroup = layer => layer.class() == "MSLayerGroup";
 // Shapes
 function filterShapes (context) {
   deselectLayers(context);
-  const selection = context.selection.slice();
-  selectLayers(selection.filter(isShape));
+  selectLayers(selectedLayers(context).filter(isShape));
 }
 
 // Groups
 function filterGroups (context) {
   deselectLayers(context);
-  const selection = context.selection.slice();
-  selectLayers(selection.filter(isGroup));
+  selectLayers(selectedLayers(context).filter(isGroup));
 }
 
 // Images
 function filterImages (context) {
   deselectLayers(context);
-  const selection = context.selection.slice();
-  selectLayers(selection.filter(isImage));
+  selectLayers(selectedLayers(context).filter(isImage));
 }
 
 // Text
 function filterTexts (context) {
   deselectLayers(context);
-  const selection = context.selection.slice();
-  selectLayers(selection.filter(isText));
+  selectLayers(selectedLayers(context).filter(isText));
 }
