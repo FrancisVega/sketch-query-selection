04be6c33086c1fc5fda0343a914aaca4d74a249e
Francis <hisconer@gmail.com>
2017-04-21 13:34:44 +0200

    Some func comments and bump version

--- README.md
+++ README.md
@@ -8,10 +8,8 @@ Groups, Shapes, Text and Image layers.
   <img width="160" height="40" src="http://sketchrunner.com/img/badge_blue.png" >
 </a>
 
-
 ## Install
 
 [![Install Sketch Query Selection with Sketchpacks](http://sketchpacks-com.s3.amazonaws.com/assets/badges/sketchpacks-badge-install.png "Install Sketch Query Selection with Sketchpacks")](https://sketchpacks.com/FrancisVega/sketch-query-selection/install)
 
 Or download and double click in query-selection.sketchplugin
-
--- query-selection.sketchplugin/Contents/Sketch/query-selection.js
+++ query-selection.sketchplugin/Contents/Sketch/query-selection.js
@@ -1,31 +1,88 @@
-// Functions
-const selectLayers = layers => layers.slice().map(layer => layer.select_byExpandingSelection(true, true));
-const deselectLayers = context => context.document.currentPage().deselectAllLayers();
-const selectedLayers = context => context.selection.slice();
-const isShape = layer => layer.class() == "MSShapeGroup";
-const isText = layer => layer.class() == "MSTextLayer";
-const isImage = layer => layer.class() == "MSBitmapLayer";
-const isGroup = layer => layer.class() == "MSLayerGroup";
+/**
+ * Select layers (adding) in Sketch from an array of layers.
+ * @param {array} layers - The array of layers
+*/
+const selectLayers = layers =>
+    layers.slice().map(layer => layer.select_byExpandingSelection(true, true));
 
-// Shapes
+/**
+ * Unselect all layers
+ * @param {object} context - Sketch Context
+ */
+const deselectLayers = context =>
+  context.document.currentPage().deselectAllLayers();
+
+/**
+ * Return current selected layers
+ * @param {object} context - Sketch Context
+ * @returns {array}
+ */
+const selectedLayers = context =>
+  context.selection.slice();
+
+/**
+ * Return true if the layer is a Shape Layer
+ * @param {object} layer - Layer object
+ * @returns {boolean}
+ */
+const isShape = layer =>
+  layer.class() == "MSShapeGroup";
+
+/**
+ * Return true if the layer is a Text Layer
+ * @param {object} layer - Layer object
+ * @returns {boolean}
+ */
+const isText = layer =>
+  layer.class() == "MSTextLayer";
+
+/**
+ * Return true if the layer is a Image Layer
+ * @param {object} layer - Layer object
+ * @returns {boolean}
+ */
+const isImage = layer =>
+  layer.class() == "MSBitmapLayer";
+
+/**
+ * Return true if the layer is a Group Layer
+ * @param {object} layer - Layer object
+ * @returns {boolean}
+ */
+const isGroup = layer =>
+  layer.class() == "MSLayerGroup";
+
+/**
+ * Filter Shape Layers
+ * @param {object} context - Sketch Context
+ */
 function filterShapes (context) {
   deselectLayers(context);
   selectLayers(selectedLayers(context).filter(isShape));
 }
 
-// Groups
+/**
+ * Filter Groups Layers
+ * @param {object} context - Sketch Context
+ */
 function filterGroups (context) {
   deselectLayers(context);
   selectLayers(selectedLayers(context).filter(isGroup));
 }
 
-// Images
+/**
+ * Filter Image Layers
+ * @param {object} context - Sketch Context
+ */
 function filterImages (context) {
   deselectLayers(context);
   selectLayers(selectedLayers(context).filter(isImage));
 }
 
-// Text
+/**
+ * Filter Text Layers
+ * @param {object} context - Sketch Context
+ */
 function filterTexts (context) {
   deselectLayers(context);
   selectLayers(selectedLayers(context).filter(isText));
